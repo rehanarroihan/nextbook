2b03b71df84ec2e1eaf2e86b08ce21064ad34cb9
setiawan1999 <setiawan1999.sw@gmail.com>
2018-01-10 17:50:16 +0700

    add facebook analytics

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -252,6 +252,25 @@
                 timer: 2000
             });            
         <?php endif; ?>
+
+        window.fbAsyncInit = function() {
+            FB.init({
+              appId      : '124182608227692',
+              xfbml      : true,
+              version    : 'v2.10'
+            });
+          
+            FB.AppEvents.logPageView();
+          
+        };
+
+        (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     </script>
 
 </html>
